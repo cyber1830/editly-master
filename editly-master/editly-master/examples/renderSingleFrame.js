985b2f560498bddbb6a54712818b508ac657ff40
cyber1830 <73875376+cyber1830@users.noreply.github.com>
2024-01-21 22:15:44 +0530

    Add files via upload

---
+++ editly-master/editly-master/examples/renderSingleFrame.js
@@ -0,0 +1,12 @@
+import { parse } from 'json5';
+import fsExtra from 'fs-extra';
+
+// eslint-disable-next-line import/named
+import { renderSingleFrame } from '..';
+
+(async () => {
+  await renderSingleFrame({
+    time: 0,
+    clips: parse(await fsExtra.readFile('./videos.json5', 'utf-8')).clips,
+  });
+})().catch(console.error);
